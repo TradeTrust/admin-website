181a08f79785bb018ed6765e565f8b01e7d5da76
Sumit Chaudhari <sumit.nitsurat@gmail.com>
2019-10-21 14:26:42 +0800

    string bug fixed

--- src/components/createDocument/dropzoneContainer.js
+++ src/components/createDocument/dropzoneContainer.js
@@ -153,9 +153,7 @@ class DropzoneContainer extends Component {
     baseDoc.issuers.push(metaObj);
     baseDoc.name = title;
     const url = encodeURIComponent(
-      JSON.stringify(
-        `{"uri": ${SHARE_LINK_API_URL}/get/${tempData.queueNumber}#${tempData.key}}`
-      )
+      `{"uri": ${SHARE_LINK_API_URL}/get/${tempData.queueNumber}#${tempData.key}}`
     );
     baseDoc.documentUrl = `tradetrust://${url}`;
     return baseDoc;
