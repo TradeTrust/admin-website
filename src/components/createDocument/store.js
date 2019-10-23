ba9bf2cedc8255d2c7d60889b5ae017f5daa6a70
RJ <27843274+rjchow@users.noreply.github.com>
2019-10-23 09:52:31 +0800

    added imda 3 4

--- src/components/createDocument/store.js
+++ src/components/createDocument/store.js
@@ -14,6 +14,14 @@ const STORE_ADDR = [
   {
     label: "IMDA Issuer - 2",
     value: "0x95A017bAFF3383A45C88F2beEd6C77AdD927217e"
+  },
+  {
+    label: "IMDA Issuer - 3",
+    value: "0x225EAEaCA4780db6447ca7Af078054F872ca2D4F"
+  },
+  {
+    label: "IMDA Issuer - 4",
+    value: "0xFD34Ea70b575B2dC3A1F0EA9F0579b5aDB20feaa"
   }
 ];
 
